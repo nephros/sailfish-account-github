5d4019f0292df6da212a73c2b7f23f798628a80a
nephros <nemo@JollaC2>
2026-05-21 12:05:42 +0200

    Add accounts-translations-plugin
    Copied from https://github.com/sailfishos/sailfish-account-mastodon/blob/main/settings/accounts-translations-plugin/

---
+++ settings/accounts-translations-plugin/accounts-translations-plugin.pro
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2019 - 2023 Jolla Ltd.
+# SPDX-FileCopyrightText: 2026 Jolla Mobile Ltd
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+TEMPLATE = lib
+TARGET = githubaccountstranslationsplugin
+TARGET = $$qtLibraryTarget($$TARGET)
+
+MODULENAME = com/jolla/settings/accounts/github
+TARGETPATH = $$[QT_INSTALL_QML]/$$MODULENAME
+
+QT += qml
+CONFIG += plugin
+
+SOURCES += plugin.cpp
+
+target.path = $$TARGETPATH
+qmldir.files = qmldir
+qmldir.path = $$TARGETPATH
+
+INSTALLS += target qmldir
---
+++ settings/accounts-translations-plugin/plugin.cpp
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2019 - 2023 Jolla Ltd.
+// SPDX-FileCopyrightText: 2026 Jolla Mobile Ltd
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <QCoreApplication>
+#include <QLocale>
+#include <QQmlEngine>
+#include <QQmlExtensionPlugin>
+#include <QTranslator>
+#include <QtQml>
+
+class AppTranslator : public QTranslator
+{
+    Q_OBJECT
+public:
+    explicit AppTranslator(QObject *parent)
+        : QTranslator(parent)
+    {
+        qApp->installTranslator(this);
+    }
+
+    ~AppTranslator() override
+    {
+        qApp->removeTranslator(this);
+    }
+};
+
+class GitHubAccountsTranslationsPlugin : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "com.jolla.settings.accounts.github")
+
+public:
+    void initializeEngine(QQmlEngine *engine, const char *uri) override
+    {
+        Q_UNUSED(uri)
+
+        AppTranslator *engineeringEnglish = new AppTranslator(engine);
+        engineeringEnglish->load("settings-accounts-github_eng_en", "/usr/share/translations");
+
+        AppTranslator *translator = new AppTranslator(engine);
+        translator->load(QLocale(), "settings-accounts-github", "-", "/usr/share/translations");
+    }
+
+    void registerTypes(const char *uri) override
+    {
+        Q_ASSERT(QLatin1String(uri) == QLatin1String("com.jolla.settings.accounts.github"));
+        qmlRegisterUncreatableType<GitHubAccountsTranslationsPlugin>(uri, 1, 0,
+                                                                        "GitHubTranslationPlugin",
+                                                                        QString());
+    }
+};
+
+#include "plugin.moc"
